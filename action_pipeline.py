ecb0a5aa70b87ccb9cd4a59a6ee59929efe07776
peterwilliams97 <peter.williams.97@gmail.com>
2010-07-25 20:48:36 +1000

    handling missing values / masks correctly

--- action_pipeline.py
+++ action_pipeline.py
@@ -8,9 +8,14 @@ Created on 25/07/2010
 """
 
 from __future__ import division
-import  copy as CP, numpy as NP, scipy as SP, pylab as PL, ols, time, optparse, os, csv, run_weka
-from pylab import *
+import  copy, random, numpy as NP, scipy as SP, pylab as PL, ols, time, optparse, os, csv, run_weka
 
+def showArray(name, a):
+    """ Display a numpy array """
+    print name, ', shape =', a.shape
+    print a
+    print '--------------------'
+    
 def getDaysOfWeekToKeep(vector, threshold):
     """ Given a vector of daily values, determines which days are outliers
         based on threshold * median
@@ -25,11 +30,30 @@ def getDaysOfWeekToKeep(vector, threshold):
     median_day = sorted(average_for_day)[3]
     return [day for day in range(7) if average_for_day[day] >= median_day *threshold]
 
-def getTrend(vector):
-    """ Find the trend in a time-series vector
-        Assume all elements are equal time apart """
-    t = NP.arange(vector.shape[0])
-    mymodel = ols.ols(vector,t,'y',['t'])
+def getDaysOfWeekMask(days_to_keep, length):
+    """ Make a mask based on days of the week """
+    return [i % 7 in days_to_keep for i in range(length)]
+
+def applyMask1D(vector, mask):
+    """ Apply a mask to a 1d numpy array """
+    assert(vector.shape[0] == len(mask))
+    number_visible = sum([1 if x else 0 for x in mask])
+    #showArray('applyMask1D', vector)
+    y = NP.zeros(number_visible)
+    count = 0
+    for i in range(len(mask)):
+        if mask[i]:
+            y[count] = vector[i]
+            #print i, count, y[count]
+            count = count + 1
+    return y
+
+def getTrend(t, y):
+    """ Find the trend in a time-series y
+        With times vector t """
+   # t = NP.arange(vector.shape[0])
+    assert(t.shape[0] == y.shape[0])
+    mymodel = ols.ols(y,t,'y',['t'])
     return mymodel.b  # return coefficients
 
 def analyzeTimeSeries(filename, max_lag, fraction_training):
@@ -64,32 +88,79 @@ def analyzeTimeSeries(filename, max_lag, fraction_training):
     timeSeriesToMatrixCsv(regression_matrix_csv, training_time_series, max_lag)
     run_weka.runMLPTrain(regression_matrix_csv, results_filename, model_filename, True)
  
-
-def showArray(name, a):
-    """ Display a numpy array """
-    print name, ', shape =', a.shape
-    print a
-    print '--------------------'
-    
-if __name__ == '__main__':
-    vector = NP.array([1.0, 2.5, 2.8, 4.1, 5.0])
-    trend = getTrend(vector)
-    print trend
-    for i in range(vector.shape[0]):
-        v_pred = trend[0] + i * trend[1]
-        print i, vector[i], v_pred, v_pred - vector[i]
-    
-    t = NP.arange(vector.shape[0])
-    predicted = NP.array([trend[0] + i * trend[1] for i in range(vector.shape[0])])
-    s = NP.transpose(NP.vstack([vector, predicted]))
-    
+def test1():
+    vector_full = NP.array([1.0, 2.5, 2.8, 4.1, 5.1, 5.9, 6.9, 8.1])
+    vector = vector_full[:-2]
+    t =  NP.arange(vector.shape[0])
     showArray('t', t) 
-    showArray('s', s)    
+    showArray('vector', vector)    
+    
+    mask = [True] * vector.shape[0]
+    mask[2] = False
+    print 'mask', len(mask), mask
+   
+    masked_vector = applyMask1D(vector, mask)
+    masked_t = applyMask1D(vector, mask)
+    trend = getTrend(t, vector)
+    print trend
+    for i in range(masked_t.shape[0]):
+        v_pred = trend[0] + masked_t[i] * trend[1]
+        print i, masked_vector[i], v_pred, v_pred - masked_vector[i]
+    
+    predicted = NP.array([trend[0] + i * trend[1] for i in range(masked_vector.shape[0])])
+    corrected = NP.array([masked_vector[i] - predicted[i] for i in range(masked_vector.shape[0])])
+    masked_s = NP.transpose(NP.vstack([masked_vector, predicted, corrected]))
+    
+    showArray('masked_t', masked_t) 
+    showArray('masked_s', masked_s)    
     # the main axes is subplot(111) by default
-    plot(t, s)
-    axis([amin(t), amax(t), 0, amax(s) ])
+    PL.plot(masked_t, masked_s)
+    s_range = PL.amax(masked_s) - PL.amin(masked_s)
+    axis([PL.amin(masked_t), PL.amax(masked_t), PL.amin(masked_s) - s_range*0.1, PL.amax(masked_s) + s_range*0.1 ])
     xlabel('time (days)')
     ylabel('downloads')
     title('Dowloads over time')
     show()
     
+def test2():
+    number_samples = 300
+    days_to_keep = [2,3,4,5,6]
+    vector_full = NP.array([2.0 + i * 10.0/number_samples  + random.uniform(-.5, .5) for i in range(number_samples)])
+    mask_full = getDaysOfWeekMask(days_to_keep, vector_full.shape[0])
+
+    vector = vector_full[:int(vector_full.shape[0]*0.8)]
+    t = NP.arange(vector.shape[0])
+    showArray('t', t) 
+    showArray('vector', vector)    
+    
+    mask = getDaysOfWeekMask(days_to_keep, vector.shape[0])
+    print 'mask', len(mask), mask
+   
+    masked_t = applyMask1D(t, mask)
+    masked_vector = applyMask1D(vector, mask)
+    showArray('masked_t', masked_t) 
+    showArray('masked_vector', masked_vector) 
+    
+    trend = getTrend(t, vector)
+    print trend
+    for i in range(masked_t.shape[0]):
+        v_pred = trend[0] + masked_t[i] * trend[1]
+        print masked_t[i], masked_vector[i], v_pred, v_pred - masked_vector[i]
+    
+    predicted = NP.array([trend[0] + masked_t[i] * trend[1] for i in range(masked_vector.shape[0])])
+    corrected = NP.array([masked_vector[i] - predicted[i] for i in range(masked_vector.shape[0])])
+    masked_s = NP.transpose(NP.vstack([masked_vector, predicted, corrected]))
+    
+    showArray('masked_t', masked_t) 
+    showArray('masked_s', masked_s)    
+    # the main axes is subplot(111) by default
+    PL.plot(masked_t, masked_s)
+    s_range = PL.amax(masked_s) - PL.amin(masked_s)
+    PL.axis([PL.amin(masked_t), PL.amax(masked_t), PL.amin(masked_s) - s_range*0.1, PL.amax(masked_s) + s_range*0.1 ])
+    PL.xlabel('time (days)')
+    PL.ylabel('downloads')
+    PL.title('Dowloads over time')
+    PL.show()    
+        
+if __name__ == '__main__':
+    test2()
